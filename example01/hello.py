0a3a892a5f5a66d8503e12e91dae5ea9884b9588
lcq0202 <lcq0202@163.com>
2017-07-07 18:12:12 +0800

    python boolean

--- example01/hello.py
+++ example01/hello.py
@@ -1,2 +1,10 @@
 print ('hello world')
 print ('hello') #注释符号
+print ('I\'m Python was started in 1989 by "Guido".')
+print ('Python is free and easy to learn.')
+print (r'\(~_~)/ \(~_~)/') #Python中raw字符串与多行字符串
+print (u'''静夜思
+床前明月光，
+疑是地上霜。
+举头望明月，
+低头思故乡。''')
