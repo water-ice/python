2cf8d211c2a081af5f524f18fdc1e8be48fff9a3
lcq0202 <lcq0202@163.com>
2017-07-13 09:42:43 +0800

    slick

--- example05/sllice.py
+++ example05/sllice.py
@@ -6,3 +6,12 @@ print (L[2::3])
 print (L[4:50:5])
 print (L[-10:])#最后10个
 print (L[-46::5])#最后10个5的倍数
+
+def firstCharUpper(s):
+    f = s[:1].upper()
+    l = s[1:]
+    return f + l
+
+print (firstCharUpper('hello'))
+print (firstCharUpper('sunday'))
+print (firstCharUpper('september'))
