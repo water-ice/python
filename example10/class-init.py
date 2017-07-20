13580b1b779dbde1e6e05fc6b9bde2743d17d3c7
lcq0202 <lcq0202@163.com>
2017-07-20 11:58:36 +0800

    methdtype

--- example10/class-init.py
+++ example10/class-init.py
@@ -37,3 +37,18 @@ print (Person.count)
 
 p3 = Person('Tim')
 print (Person.count)
+
+
+class Person(object):
+
+    __count = 0
+
+    def __init__(self, name):
+        self.name = name
+        Person.__count = Person.__count + 1
+        print (Person.__count)
+
+p1 = Person('Bob')
+p2 = Person('Alice')
+
+#print Person.__count
---
+++ example10/methodtype.py
@@ -0,0 +1,10 @@
+class Person(object):
+
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+        self.get_grade = lambda: 'A'
+
+p1 = Person('Bob', 90)
+print p1.get_grade
+print p1.get_grade()
