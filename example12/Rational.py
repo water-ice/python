701ec728253640f908ed81f88b11881880c57126
lcq0202 <lcq0202@163.com>
2017-07-21 11:33:44 +0800

    rational

--- example12/Rational.py
+++ example12/Rational.py
@@ -26,3 +26,19 @@ print (r1 + r2)
 print (r1 - r2)
 print (r1 * r2)
 # print (r1 / r2)
+
+
+class Rational(object):
+    def __init__(self, p, q):
+        self.p = p
+        self.q = q
+
+    def __int__(self):
+        return int(self.p // self.q)
+
+    def __float__(self):
+        return float(self.p) / self.q
+
+print (float(Rational(7, 2)))
+print (int(Rational(7, 2)))
+print (float(Rational(1, 3)))
